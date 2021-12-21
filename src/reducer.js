60ce0b814c0700f79e685f6dba715b6f631ce57c
Dmitry Mass <dimamikron41@gmail.com>
2021-12-21 19:07:23 +0300

    Complete Cases

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,27 @@
-function reducer(input, action) {
-  // Ваше решение тут
+function nameCapitalize(str) {
+  let string = str.split(" ");
+
+  let result = string.map(function (item) {
+    return item[0].toUpperCase() + item.slice(1);
+  });
+  return result.join(" ");
 }
 
-module.exports = reducer;
+let string = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam dolor velit quas soluta dicta sunt nesciunt repellendus quia error minus assumenda, autem animi esse rem suscipit ut quaerat deserunt inventore.`;
+
+function reducer(input, action) {
+  // Ваше решение тут
+
+  switch (action) {
+    case "upperCase":
+      return input.toUpperCase();
+
+    case "lowerCase":
+      return input.toLowerCase();
+    case "capitalize":
+      return nameCapitalize(string);
+  }
+}
+console.log(reducer(string, "upperCase"));
+console.log(reducer(string, "lowerCase"));
+console.log(reducer(string, "capitalize"));
